a31a0cf4247494fc000f2fa1537de8f8df2dcd2a
hcvs7305041 <48470597+hcvs7305041@users.noreply.github.com>
2019-10-19 11:44:24 +0800

    Create 2015 01.cpp

---
+++ 2015 01.cpp
@@ -0,0 +1,33 @@
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int n = 0 , xx[82] = { 0 };
+	int k = 0 ,i = 0, j = 0;
+	cin >> n;
+	for (i = 2; i <= 9; i++) 
+	{
+		for (j = 1; j <= 9; j++) 
+		{
+			k = i*j;
+			if (k / 10 == n || k % 10 == n)
+			{
+				xx[k] = 1;
+			}
+		}
+	}
+	for (i = 1; i <= 81; i++) 
+	{
+		if (xx[i] == 1)
+		{
+			cout << i << "  ";
+		}
+	}
+
+	system("PAUSE");
+	return 0;
+}
+
+
